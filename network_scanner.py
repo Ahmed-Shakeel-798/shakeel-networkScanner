c32f591dfa7c1a7549d6822545b716ad4e8aec35
root <ahmed.shakeel.work@gmail.com.com>
2021-07-29 21:03:59 +0100

    scapy.ARP() and scapy.ls

--- network_scanner.py
+++ network_scanner.py
@@ -3,7 +3,13 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP()
+    print(arp_request.summary())
+    # lists the packet summary
+
+    scapy.ls(scapy.ARP())
+    # lists all the available options of a class
+
 
 scan("192.168.131.2/24")
 # /24 is a common way of specifying the whole subnet
