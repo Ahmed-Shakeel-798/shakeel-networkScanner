df6a68b05e143d837f7babb67d8756d2ea4d8ddc
root <ahmed.shakeel.work@gmail.com.com>
2021-08-01 08:30:05 +0100

    scapy.Ether() and combination if ether and arp packets

--- network_scanner.py
+++ network_scanner.py
@@ -4,9 +4,18 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    # also we can do: arp_request.pdst = ip
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+
+    # broadcast mac address is a virtual mac address that doesn't really exists but when
+    # you send something to it, all clients on the network will receive it
+
+    arp_request.show()
+    broadcast.show()
+
+    # combination of both packets we created above, so ir contains an Ether part and an ARP part
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
+    # print(arp_request_broadcast.summary())
 
 
 scan("192.168.131.2/24")
-# /24 is a common way of specifying the whole subnet
